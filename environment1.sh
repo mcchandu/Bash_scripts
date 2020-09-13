b3f1be0a4228b0f78520ab4840392ed574a63445
mcchandu <mc.chandu@gmail.com>
2020-09-13 03:25:08 +0000

    First Commit

---
+++ environment1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+clear
+
+echo "This script will give Environment information"
+
+echo "*********************************************"
+
+echo ""
+
+echo "Hello Username: $USER"
+
+echo ""
+
+echo "SYSTEM HOSTNAME: $HOSTNAME"
+
+echo ""
+
+echo "Current Shell: $SHELL"
+
+
+
+
